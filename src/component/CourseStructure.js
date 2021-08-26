d03fc94b3ee5e36c693d9a191dde864dd8fb3d9f
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-26 20:56:07 +0700

    dokumen tutorial menambahkan youtube api key dan membuat playlist youtube menggunakan api key

--- src/component/CourseStructure.js
+++ src/component/CourseStructure.js
@@ -1,55 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CourseStructure(props) {
   const courseName = props.match.params.coursename;
-  const courses = {
-    reactjs: [
-      { title: "react js title 1", vid: "edblCnJhvdI" },
-      { title: "react js title 2", vid: "o8jEBYOu9dA" },
-      { title: "react js title 3", vid: "o8jEBYOu9dA" },
-    ],
-    nodejs: [
-      { title: "node js title 1", vid: "F-i0d67PVkE" },
-      { title: "node js title 2", vid: "o8jEBYOu9dA" },
-      { title: "node js title 3", vid: "o8jEBYOu9dA" },
-    ],
-  };
+  const [courses, setCourses] = useState([]);
+  const [vid, uid] = useState("");
+  const [title, utit] = useState("");
+  const [counter, setCounter] = useState(0);
 
-  const [vid, uid] = useState(courses[courseName][0].vid);
-  const [title, utit] = useState(courses[courseName][0].title);
-  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    let playlistId = "";
+    if (courseName === "reactjs") {
+      playlistId = "PLZQbl9Jhl-VDeCuNNp7C2SR1lFsIjQRQo";
+    } else {
+      playlistId = "PLCZlgfAG0GXDsbx5eHX0dFzF2C35THONn";
+    }
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2Csnippet&maxResults=10&playlistId=${playlistId}&key=AIzaSyCVZ4rPoObY5zIiG3PyBIBT2M7t5V2vBqs`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("nyari data", data);
+        const result = data.items.map((item) => {
+          return {
+            title: item.snippet.title,
+            vid: item.contentDetails.videoId,
+          };
+        });
+        setCourses(result);
+        uid(result[0].vid);
+        utit(result[0].title);
+      });
+  }, [courseName]);
 
   const renderVideo = () => {
     return (
       <>
         <h1>{title}</h1>
         <div className="video-container">
-            <iframe
+          <iframe
+            title={counter}
             width={853}
             height={480}
             src={`//www.youtube.com/embed/${vid}?rel=0`}
             frameBorder={0}
             allowFullScreen
-            />
+          />
         </div>
       </>
     );
   };
+
   return (
-    <div>
-      {renderVideo()}
-      <ul className="collection">
-        {
-            courses[courseName].map((e, i) => {
-                return <li key={e.vid} className={counter === i ? "collection-item myitem" : "collection-item"} onClick={() => {
-                    uid(e.vid)
-                    utit(e.title)
-                    setCounter(i)
-                }}>{e.title}</li>
-            })
-        }
-      </ul>
-    </div>
+    <>
+      {courses.length > 0 ? (
+        <div>
+          {renderVideo()}
+          <ul className="collection">
+            {courses.map((e, i) => {
+              return (
+                <li
+                  key={e.vid}
+                  className={
+                    counter === i ? "collection-item myitem" : "collection-item"
+                  }
+                  onClick={() => {
+                    uid(e.vid);
+                    utit(e.title);
+                    setCounter(i);
+                  }}
+                >
+                  {e.title}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
   );
 }
 
